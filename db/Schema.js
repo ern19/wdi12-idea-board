8696895835f66e7c1376e0e563c40495c5dd6108
ern19 <aarontrammell19@gmail.com>
2017-10-12 12:14:18 -0400

    made db, schema

---
+++ db/Schema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const IdeaSchema = new Schema({
+    title: {
+        type: String,
+        default: "New Idea Title",
+        required: true
+    },
+    description: {
+        type: String,
+        default: "New Idea Description",
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+})
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    ideas: [IdeaSchema]
+})
+
+const IdeaModel = mongoose.model("Idea", IdeaSchema)
+const UserModel = mongoose.model("User", UserSchema)
+
+module.exports = {
+    IdeaModel: IdeaModel,
+    UserModel: UserModel
+}
